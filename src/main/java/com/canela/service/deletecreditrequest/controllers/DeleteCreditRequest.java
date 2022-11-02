da1ccde12694d855ac1bc02cc676ec27faabefc6
Brayan Estiben Giraldo Lopez <60472583+estigol@users.noreply.github.com>
2022-11-02 11:13:09 -0500

    Update DeleteCreditRequest.java

--- src/main/java/com/canela/service/deletecreditrequest/controllers/DeleteCreditRequest.java
+++ src/main/java/com/canela/service/deletecreditrequest/controllers/DeleteCreditRequest.java
@@ -1,25 +1,31 @@
 package com.canela.service.deletecreditrequest.controllers;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/credit")
 public class DeleteCreditRequest {
     @DeleteMapping("/request")
-    public ResponseEntity<String> delete (@PathVariable String cRequestId) {
+    public ResponseEntity<String> delete (@RequestBody String requestBody) throws JsonProcessingException {
+        Map<String, Object> values = new ObjectMapper().readValue(requestBody, HashMap.class);
+        String cRequestId = (String) values.get("cRequestId");
+
         URL url = null;
         try {
-            url = new URL("http://localhost:4000/graphql?query=mutation{deleteCreditRequest(crId:\"" + cRequestId + "\")}");
+            url = new URL("http://localhost:8080/graphql?query=mutation{deleteCreditRequest(crId:\"" + cRequestId + "\")}");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("DELETE");
             int response = conn.getResponseCode();
