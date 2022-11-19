6a8a3598ff96acdc2a1be5ff90caca57bd738470
Carlos Loreto <sa_carlos@javeriana.edu.co>
2022-11-19 13:11:45 -0500

    Delete unused imports

--- src/main/java/com/canela/service/loanmgmt/controllers/GetLoansControllers.java
+++ src/main/java/com/canela/service/loanmgmt/controllers/GetLoansControllers.java
@@ -14,10 +14,8 @@ import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
