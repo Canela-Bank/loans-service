619526df4dfa3c8f2cbfca2fcf7c4b17025f8b36
estefania_bermudeza <estefanibearroyo@gmail.com>
2022-11-20 18:52:08 -0500

    Fix data types

--- src/main/java/com/canela/service/loanmgmt/controllers/CreditRequestController.java
+++ src/main/java/com/canela/service/loanmgmt/controllers/CreditRequestController.java
@@ -17,6 +17,7 @@ import java.net.ProtocolException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/api/loans")
@@ -32,23 +33,24 @@ public class CreditRequestController {
             JSONObject user = (JSONObject) clientObject.get("User");
             String type = (String) user.get("type");
             int document = (int) user.get("document");
-            url = new URL ("http://10.2.0.0:3000/api/prov/centralderiesgo/getReports/"+ type +"/"+ document +"");
+            url = new URL ("http://localhost:3000/api/prov/centralderiesgo/getReports/"+ type +"/"+ document +"");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
             int responseCode = con.getResponseCode();
             if(responseCode == HttpURLConnection.HTTP_OK) {
 //                return new ResponseEntity(con.getResponseMessage(),HttpStatus.OK);
                 JSONObject loan = (JSONObject) clientObject.get("Loan");
-                String id = (String) loan.get("id");
+                Random random = new Random();
+                String id = String.valueOf(random.nextLong(1000000000L));
                 Double interest_rate = (Double) loan.get("interest_rate");
-                Double min_payment = (Double) loan.get("min_payment");
-                Double balance = (Double) loan.get("balance");
+                Double min_payment = Double.valueOf(loan.get("min_payment").toString());
+                Double balance = Double.valueOf(loan.get("balance").toString());
                 String payment_date = (String) loan.get("payment_date");
-                Double debt = (Double) loan.get("debt");
+                Double debt = Double.valueOf(loan.get("debt").toString());
                 String user_id = (String) loan.get("user_id");
-                Integer user_document_type = (Integer) loan.get("user_document_type");
+                Integer user_document_type = Integer.valueOf(loan.get("user_document_type").toString());
                 try{
-                    url = new URL("http://10.1.0.0:3001/graphql?query=mutation%7B%0A%20%20createLoan(id%3A%22"+id+"%22%2Cinterest_rate%3A"+interest_rate+"%2C%20min_payment%3A"+min_payment+"%2Cbalance%3A"+balance+"%2Cpayment_date%3A%22"+payment_date+"%22%2Cdebt%3A"+debt+"%2Cuser_id%3A%22"+user_id+"%22%2Cuser_document_type%3A"+user_document_type+")%7B%0A%20%20%20%20id%0A%20%20%7D%0A%7D");
+                    url = new URL("http://localhost:3002/graphql?query=mutation%7B%0A%20%20createLoan(id%3A%22"+id+"%22%2Cinterest_rate%3A"+interest_rate+"%2C%20min_payment%3A"+min_payment+"%2Cbalance%3A"+balance+"%2Cpayment_date%3A%22"+payment_date+"%22%2Cdebt%3A"+debt+"%2Cuser_id%3A%22"+user_id+"%22%2Cuser_document_type%3A"+user_document_type+")%7B%0A%20%20%20%20id%0A%20%20%7D%0A%7D");
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                     conn.setRequestMethod("POST");
                     int response = conn.getResponseCode();
@@ -84,7 +86,7 @@ public class CreditRequestController {
 
         URL url = null;
         try {
-            url = new URL("http://10.1.0.0:3001/graphql?query=mutation%20%7B%0A%20%20deleteLoan(id%3A%20\"" + cRequestId + "\")%20%7B%0A%20%20%20%20message%0A%20%20%7D%0A%7D");
+            url = new URL("http://localhost:3002/graphql?query=mutation%20%7B%0A%20%20deleteLoan(id%3A%20\"" + cRequestId + "\")%20%7B%0A%20%20%20%20message%0A%20%20%7D%0A%7D");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("DELETE");
             int response = conn.getResponseCode();
